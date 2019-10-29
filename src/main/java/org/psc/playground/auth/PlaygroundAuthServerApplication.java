6c89ade53e116669ffb6b5b3c49c4e738e306e79
sykq <genericmailacc@gmail.com>
2019-10-29 18:27:58 +0100

    init

---
+++ src/main/java/org/psc/playground/auth/PlaygroundAuthServerApplication.java
@@ -0,0 +1,13 @@
+package org.psc.playground.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/psc/playground/auth/PlaygroundAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psc.playground.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
